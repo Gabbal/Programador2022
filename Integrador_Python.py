46446334de3299d0c783ab258789b5c939e82237
gex12 <gonzaloabelolmedo8@gmail.com>
2022-08-13 17:34:58 -0700

    Agrego integrador python

---
+++ Integrador_Python.py
@@ -0,0 +1,18 @@
+## Integrador Introducción a la Programación
+
+## Actividad 1
+
+Dueno = [28957346, 'Juan', 'Perez', 4789689, 'Belgrano 101']
+
+entero = 26000000
+
+if Dueno[0] > 26000000:
+    print('El número de teléfono es:' , Dueno[3])
+
+## Actividad 2  
+
+Dueno2 = [23546987, "Maria",  "Perez", 4789689, "Pueyrredon  811"]
+
+for i in range(len(Dueno2)):
+	if i != 0 and i != 2:
+		print(Dueno2[i])
